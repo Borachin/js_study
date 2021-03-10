0ebe0fec3a0b535eb464c3800ab5e04d1c0b2f13
Borachin <chinbora92@naver.com>
2021-03-11 02:34:51 +0900

    수정

--- study1/main.js
+++ study1/main.js
@@ -7,9 +7,11 @@ function randomValueFromArray(array){
   return array[random];
 }
 let storyText = 'It was 94 fahrenheit outside, so Willy the Goblin went for a walk. When they got to Disneyland, they stared in horror for a few moments, then spontaneously combusted. Bob saw the whole thing, but was not surprised — Willy the Goblin weighs 300 pounds, and it was a hot day. ';
-let insertX =['Willy the Goblin , Big Daddy, Father Christmas'];
-let insertY = ['the soup kitchen , Disneyland , the White House'];
-let insertZ = ['spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away'];
+let insertX =('Willy the Goblin , Big Daddy, Father Christmas');
+let insertY = ('the soup kitchen , Disneyland , the White House');
+let insertZ = ('spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away');
+
+
 
 
 randomize.addEventListener('click', result);
@@ -22,17 +24,16 @@ function result() {
     let yItem = randomValueFromArray(insertY)
     let zItem = randomValueFromArray(insertZ)
 
-  if(customName.value !== '') {
-    let name = customName.value;
+    if(customName.value !== '') {
+        let name = customName.value;
+    }
 
-  }
+    if(document.getElementById("uk").checked) {
+        let weight = Math.round(300);
+        let temperature =  Math.round(94);
 
-  if(document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature =  Math.round(94);
+    }
 
-  }
-
-  story.textContent = ;
-  story.style.visibility = 'visible';
+    story.textContent = ;
+    story.style.visibility = 'visible';
 }
