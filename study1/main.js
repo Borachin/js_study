78f92e29279c7ac8cef4212abf8427574ac75db3
Borachin <chinbora92@naver.com>
2021-03-13 23:19:21 +0900

    main.js 기초 작동완료

--- study1/main.js
+++ study1/main.js
@@ -10,7 +10,7 @@ function randomValueFromArray(array) {
     return array[random];
 }
 
-let newstory = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+let start = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 let insertX = ('Willy the Goblin , Big Daddy, Father Christmas');
 let insertY = ('the soup kitchen , Disneyland , the White House');
 let insertZ = ('spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away');
@@ -24,22 +24,25 @@ function result() {
     let yItem = randomValueFromArray(insertY);
     let zItem = randomValueFromArray(insertZ);
 
-    newStory = placeholder(':insertx:',xItem);
-    newStory = placeholder(':insertY:',yItem);
-    newStory = placeholder(':insertZ:',zItem);
+    newStory = newStory.replace(':insertx:',xItem);
+    newStory = newStory.replace(':insertY:',yItem);
+    newStory = newStory.replace(':insertZ:',zItem);
+
 
     if (customName.value !== '') {
         let name = customName.value;
 
         newStory = newStory.replace('Bob', name);
+        // "만약 customName 텍스트 입력에 값이 들어가 있다면, 이야기속의 Bob을 사용자가 정의한 이름으로 바꾸세요"
     }
 
     if (document.getElementById("uk").checked) {
-        let weight = Math.round(300);
-        let temperature = Math.round(94);
+        let weight = Math.round(300) + 'stone';
+        let temperature = Math.round(94) + 'centigrade';
 
+        newStory = newStory.replace('94 farenheit', temperature);
+        newStory = newStory.replace('300 pounds', weight);
     }
-
-    story.textContent = ;
+    story.textContent = newStory;
     story.style.visibility = 'visible';
 }
