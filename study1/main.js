8c7a31e3c8b3b7e327b8b724aa70461aebb9e0fd
Borachin <59508289+Borachin@users.noreply.github.com>
2021-03-13 23:50:19 +0900

    Update main.js
    let newStory = storyText

--- study1/main.js
+++ study1/main.js
@@ -31,7 +31,7 @@ randomize.addEventListener('click', result);
 
 function result() {
 
-    let newStory = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+    let newStory = storyText;
     let xItem = randomValueFromArray(insertx);
     let yItem = randomValueFromArray(insertx);
     let zItem = randomValueFromArray(insertx);
