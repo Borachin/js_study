5b22b80af3a63d9585b5ea9e1e95a019e9b8d262
Borachin <chinbora92@naver.com>
2021-03-14 00:01:42 +0900

    story_section 수정

--- study1/main.js
+++ study1/main.js
@@ -10,17 +10,15 @@ function randomValueFromArray(array) {
     return array[random];
 }
 
-let start = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+let startStory = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
+
 // let insertx = ['Willy the Goblin, Big Daddy, Father Christmas'];
 // let inserty = ['the soup kitchen, Disneyland, the White House'];
 // let insertZ = ['spontaneously combusted, melted into a puddle on the sidewalk, turned into a slug and crawled away'];
 
-
 // Q1.
 // ↑ 위에 선언된 것을 사용했을때는 이름이 적용되는데 스토리가 랜덤으로 안 바뀌고 
 // ↓ 아래 코드를 입력했을때는 이름값은 적용이 안되는데 스토리는 랜덤으로 돌아가요.
-
-let startStory = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 
 let insertx = ('Willy the Goblin, Big Daddy, Father Christmas');
 let inserty = ('the soup kitchen, Disneyland, the White House');
@@ -36,10 +34,10 @@ function result() {
     let yItem = randomValueFromArray(insertx);
     let zItem = randomValueFromArray(insertx);
 
-    newStory = newStory.replace(':insertx:',xItem);
-    newStory = newStory.replace(':inserty:',yItem);
-    newStory = newStory.replace(':insertz:',zItem);
-    newStory = newStory.replace(':insertx:',xItem);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
+    newStory = newStory.replace(':insertx:', xItem);
 
 
     if (customName.value !== '') {
@@ -54,7 +52,7 @@ function result() {
 
         newStory = newStory.replace('300 pounds', weight);
         newStory = newStory.replace('94 farenheit', temperature);
-        
+
         // Q2. 왜 'centigrade'로 안 바뀌는거일까요?
         // Q2. 수치값을 다르게 보여주려면 어떻게 해야되나요?
     }
